5912af29defb27d84f34b86826f9a05f64d91f4d
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-09-20 18:20:03 +0200

    ♻️ Mindre delt tilstand i VurderÅpenGosysoppgaveTest
    I stedet for å ha en instans av CommandContext som en var i klassen,
    oppretter hver test sin egen context.
    
    Gjør det lettere å følge flyten i en test.

--- spesialist-selve/src/test/kotlin/no/nav/helse/modell/gosysoppgaver/VurderÅpenGosysoppgaveTest.kt
+++ spesialist-selve/src/test/kotlin/no/nav/helse/modell/gosysoppgaver/VurderÅpenGosysoppgaveTest.kt
@@ -16,7 +16,6 @@ import no.nav.helse.modell.vedtaksperiode.GenerasjonDto
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -41,56 +40,24 @@ internal class VurderÅpenGosysoppgaveTest {
     private val dao = mockk<ÅpneGosysOppgaverDao>(relaxed = true)
     private val oppgaveService = mockk<OppgaveService>(relaxed = true)
 
-    private fun command(
-        harTildeltOppgave: Boolean = false,
-    ) = VurderÅpenGosysoppgave(
-        AKTØR_ID,
-        dao,
-        VEDTAKPERIODE_ID_AG_1,
-        sykefraværstilfelle,
-        harTildeltOppgave = harTildeltOppgave,
-        oppgaveService = oppgaveService,
-    )
-
-    private lateinit var context: CommandContext
-
-    private val observer =
-        object : CommandContextObserver {
-            val behov = mutableMapOf<String, Map<String, Any>>()
-
-            override fun behov(
-                behov: String,
-                ekstraKontekst: Map<String, Any>,
-                detaljer: Map<String, Any>,
-            ) {
-                this.behov[behov] = detaljer
-            }
-
-            override fun hendelse(hendelse: String) {}
-        }
-
-    @BeforeEach
-    fun setup() {
-        context = CommandContext(UUID.randomUUID())
-        context.nyObserver(observer)
-        clearMocks(dao)
-    }
-
     @Test
     fun `Ber om åpne oppgaver i gosys`() {
+        val behov = mutableMapOf<String, Map<String, Any>>()
+        val context = commandContext(behov)
         assertFalse(command().execute(context))
-        assertEquals(listOf("ÅpneOppgaver"), observer.behov.keys.toList())
-        assertEquals(skjæringstidspunkt.minusYears(1), observer.behov["ÅpneOppgaver"]!!["ikkeEldreEnn"])
+        assertEquals(listOf("ÅpneOppgaver"), behov.keys.toList())
+        assertEquals(skjæringstidspunkt.minusYears(1), behov["ÅpneOppgaver"]!!["ikkeEldreEnn"])
     }
 
     @Test
     fun `Mangler løsning ved resume`() {
-        assertFalse(command().resume(context))
+        assertFalse(command().resume(commandContext()))
         verify(exactly = 0) { dao.persisterÅpneGosysOppgaver(any()) }
     }
 
     @Test
     fun `Lagrer løsning ved resume`() {
+        val context = commandContext()
         context.add(ÅpneGosysOppgaverløsning(LocalDateTime.now(), FNR, 0, false))
         assertTrue(command().resume(context))
         verify(exactly = 1) { dao.persisterÅpneGosysOppgaver(any()) }
@@ -102,8 +69,12 @@ internal class VurderÅpenGosysoppgaveTest {
         generasjonAg1.inspektør {
             assertEquals(1, varsler.size)
         }
-        context.add(ÅpneGosysOppgaverløsning(LocalDateTime.now(), FNR, 0, false))
-        assertTrue(command().resume(context))
+
+        commandContext().let { commandContext ->
+            commandContext.add(ÅpneGosysOppgaverløsning(LocalDateTime.now(), FNR, 0, false))
+            assertTrue(command().resume(commandContext))
+        }
+
         verify(exactly = 1) { dao.persisterÅpneGosysOppgaver(any()) }
         generasjonAg1.inspektør {
             assertEquals(1, varsler.size)
@@ -116,6 +87,7 @@ internal class VurderÅpenGosysoppgaveTest {
     @Test
     fun `Deaktiverer ikke varsel dersom oppgave er tildelt`() {
         generasjonAg1.håndterNyttVarsel(Varsel(UUID.randomUUID(), "SB_EX_1", LocalDateTime.now(), VEDTAKPERIODE_ID_AG_1))
+        val context = commandContext()
         context.add(ÅpneGosysOppgaverløsning(LocalDateTime.now(), FNR, 0, false))
         assertTrue(command(harTildeltOppgave = true).resume(context))
         generasjonAg1.inspektør {
@@ -127,14 +99,15 @@ internal class VurderÅpenGosysoppgaveTest {
 
     @Test
     fun `Lagrer varsel ved åpne oppgaver, uavhengig om eventuell oppgave er tildelt eller ikke`() {
-        lagrerVarselVedÅpneOppgaver(false)
+        lagrerVarselVedÅpneOppgaver(harTildeltOppgave = false, commandContext())
         verify(exactly = 1) { oppgaveService.leggTilGosysEgenskap(any()) }
-        setup()
-        lagrerVarselVedÅpneOppgaver(true)
+        clearMocks(dao)
+        lagrerVarselVedÅpneOppgaver(true, commandContext())
     }
 
     @Test
     fun `Lagrer varsel ved oppslag feilet`() {
+        val context = commandContext()
         context.add(ÅpneGosysOppgaverløsning(LocalDateTime.now(), FNR, null, true))
         assertTrue(command().resume(context))
         verify(exactly = 1) { dao.persisterÅpneGosysOppgaver(any()) }
@@ -147,6 +120,7 @@ internal class VurderÅpenGosysoppgaveTest {
     @Test
     fun `Legger ikke til egenskap for gosys dersom det er andre varsler på perioden`() {
         generasjonAg1.håndterNyttVarsel(Varsel(UUID.randomUUID(), "SB_EX_4", LocalDateTime.now(), VEDTAKPERIODE_ID_AG_1))
+        val context = commandContext()
         context.add(ÅpneGosysOppgaverløsning(LocalDateTime.now(), FNR, 1, false))
         command().resume(context)
         verify(exactly = 0) { oppgaveService.leggTilGosysEgenskap(any()) }
@@ -155,12 +129,13 @@ internal class VurderÅpenGosysoppgaveTest {
     @Test
     fun `Legger ikke til egenskap for gosys dersom det er andre varsler på andre overlappende perioder`() {
         generasjonAg2.håndterNyttVarsel(Varsel(UUID.randomUUID(), "SB_EX_4", LocalDateTime.now(), VEDTAKPERIODE_ID_AG_2))
+        val context = commandContext()
         context.add(ÅpneGosysOppgaverløsning(LocalDateTime.now(), FNR, 1, false))
         command().resume(context)
         verify(exactly = 0) { oppgaveService.leggTilGosysEgenskap(any()) }
     }
 
-    private fun lagrerVarselVedÅpneOppgaver(harTildeltOppgave: Boolean) {
+    private fun lagrerVarselVedÅpneOppgaver(harTildeltOppgave: Boolean, context: CommandContext) {
         context.add(ÅpneGosysOppgaverløsning(LocalDateTime.now(), FNR, 1, false))
         assertTrue(command(harTildeltOppgave).resume(context))
         verify(exactly = 1) { dao.persisterÅpneGosysOppgaver(any()) }
@@ -179,6 +154,32 @@ internal class VurderÅpenGosysoppgaveTest {
             skjæringstidspunkt = 1.januar,
         )
 
+    private fun command(
+        harTildeltOppgave: Boolean = false,
+    ) = VurderÅpenGosysoppgave(
+        AKTØR_ID,
+        dao,
+        VEDTAKPERIODE_ID_AG_1,
+        sykefraværstilfelle,
+        harTildeltOppgave = harTildeltOppgave,
+        oppgaveService = oppgaveService,
+    )
+
+    private fun commandContext(behovsamler: MutableMap<String, Map<String, Any>>? = null) =
+        CommandContext(UUID.randomUUID()).also { cc ->
+            behovsamler?.let { cc.nyObserver(observer(behovsamler)) }
+        }
+
+    private fun observer(behovsamler: MutableMap<String, Map<String, Any>>) =
+        object : CommandContextObserver {
+            override fun behov(
+                behov: String,
+                ekstraKontekst: Map<String, Any>,
+                detaljer: Map<String, Any>,
+            ) {
+                behovsamler[behov] = detaljer
+            }
+        }
 }
 
 internal fun Generasjon.inspektør(block: GenerasjonDto.() -> Unit) {
